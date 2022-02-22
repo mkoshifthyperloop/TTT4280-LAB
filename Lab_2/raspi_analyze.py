f1049be1dc3d106b776653a60a74352940db7a8c
Magnus Oddstoel <magnus.oddstoel@ubiqaerospace.com>
2022-02-22 13:58:13 +0100

    added test data from array

---
+++ Lab_2/raspi_analyze.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as signal
+
+
+def raspi_import(path, channels=5):
+    """
+    Import data produced using adc_sampler.c.
+    Returns sample period and ndarray with one column per channel.
+    Sampled data for each channel, in dimensions NUM_SAMPLES x NUM_CHANNELS.
+    """
+
+    with open(path, 'r') as fid:
+        sample_period = np.fromfile(fid, count=1, dtype=float)[0]
+        data = np.fromfile(fid, dtype=np.uint16)
+        data = data.reshape((-1, channels))
+    return sample_period, data
+
+
+# Import data from bin file
+sample_period, data = raspi_import('1khzSine5s.bin')
+
+#data = signal.detrend(data, axis=0)  # removes DC component for each channel
+sample_period *= 1e-6  # change unit to micro seconds
+
+data = data/4096*3.3-3.3/2
+
+# Generate time axis
+num_of_samples = data.shape[0]  # returns shape of matrix
+t = np.linspace(start=0, stop=num_of_samples*sample_period, num=num_of_samples)
+
+# Generate frequency axis and take FFT
+freq = np.fft.fftfreq(n=num_of_samples, d=sample_period)
+spectrum = np.fft.fft(data, axis=0)  # takes FFT of all channels
+
+
+# Plot the results in two subplots
+# NOTICE: This lazily plots the entire matrixes. All the channels will be put into the same plots.
+# If you want a single channel, use data[:,n] to get channel n
+plt.subplot(2, 1, 1)
+plt.title("Time domain signal")
+plt.xlabel("Time [us]")
+plt.ylabel("Voltage")
+plt.plot(t, data[:,0])
+plt.plot(t, data[:,1])
+plt.plot(t, data[:,2])
+
+plt.subplot(2, 1, 2)
+plt.title("Power spectrum of signal")
+plt.xlabel("Frequency [Hz]")
+plt.ylabel("Power [dB]")
+plt.plot(freq, 20*np.log10(np.abs(spectrum[:,0]))) # get the power spectrum
+plt.plot(freq, 20*np.log10(np.abs(spectrum[:,1])))
+plt.plot(freq, 20*np.log10(np.abs(spectrum[:,2])))
+
+plt.show()
